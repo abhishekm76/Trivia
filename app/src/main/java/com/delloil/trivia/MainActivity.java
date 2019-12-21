6ac780577d1961481ed353da9fc2d23fca4a5553
Abhishek <abhishek.amishra@gmail.com>
2019-12-21 21:05:26 +0530

    QB part2

--- app/src/main/java/com/delloil/trivia/MainActivity.java
+++ app/src/main/java/com/delloil/trivia/MainActivity.java
@@ -14,19 +14,22 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "main" ;
+    private static final String TAG = "trackmain" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+        List<Question> questionList = new QuestionBank().getQuestions(
+
+                new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
                 Log.d(TAG, "processFinished: "+questionArrayList);
             }
-        });
+        }
+        );
         Log.d(TAG, "onCreate: "+questionList);
     }
 }
